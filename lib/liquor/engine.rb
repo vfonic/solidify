23453cbb11909191aed90691b482a1917476bd9d
Viktor Fonic <viktor.fonic@gmail.com>
2017-01-30 18:44:34 +0700

    Use rspec as test framework

--- lib/liquor/engine.rb
+++ lib/liquor/engine.rb
@@ -2,5 +2,9 @@ module Liquor
   class Engine < Rails::Engine
     isolate_namespace Liquor
     engine_name 'liquor'
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
