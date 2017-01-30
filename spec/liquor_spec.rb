bfa9d351f7a12df03f174035c2fa5a3a6397b0c8
Viktor Fonic <viktor.fonic@gmail.com>
2017-01-30 18:12:50 +0700

    Initial commit

---
+++ spec/liquor_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Liquor do
+  it "has a version number" do
+    expect(Liquor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
