ed56a1e8beb7492f2d3195178ad1166bd694176f
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-08 09:57:35 +0700

    Use local file storage for carrierwave in dev

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.development?
+  if Rails.env.production?
     config.fog_provider = 'fog/aws'
 
     config.fog_credentials = {
