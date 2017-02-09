aa6cb5e3dd587433f0a1e8c38b19cdf0f6aebaf9
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-09 17:35:33 +0700

    Add first_name and last_name to spree_users

---
+++ db/migrate/20170209103022_add_attributes_to_spree_users.rb
@@ -0,0 +1,6 @@
+class AddAttributesToSpreeUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :spree_users, :first_name, :string
+    add_column :spree_users, :last_name, :string
+  end
+end
