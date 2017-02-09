bebefd611e937b67d060e5dab7240fab8e79f52c
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-09 17:34:41 +0700

    Use SolidusLiquid::HomeController

---
+++ app/controllers/solidus_liquid/home_controller.rb
@@ -0,0 +1,16 @@
+module SolidusLiquid
+  class HomeController < LiquidController
+
+    def index
+      render controller_action_to_liquid_file_path
+    end
+
+    def set_liquid_assigns
+      @liquid_assigns = {
+        'canonical_url' => request.base_url.to_s,
+        'page_title' => '',
+        'template' => 'page'
+      }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 SolidusLiquid::Engine.routes.draw do
+  root to: 'home#index'
   get 'pages/:id', to: 'pages#show', as: :page
 end
