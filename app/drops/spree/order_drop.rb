79e34402205aa6c6b9726d323eff3288c04816bd
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-09 14:25:01 +0700

    Add several Order methods to OrderDrop for use in liquid templates

--- app/drops/spree/order_drop.rb
+++ app/drops/spree/order_drop.rb
@@ -1,5 +1,10 @@
 module Spree
   class OrderDrop < ::Liquid::Rails::Drop
+    attributes :item_count, :total
+
     belongs_to :customer
+
+    # TODO: make sure solidus total shows the same as shopfiy's total_price
+    alias total_price total
   end
 end
