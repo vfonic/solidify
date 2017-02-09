adaba654ea8185b0a392eeeb4fa7e4a2d8fe9a60
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-09 17:51:22 +0700

    Fix logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,16 @@ SolidusLiquid::Engine.routes.draw do
   root to: 'home#index'
   get 'pages/:id', to: 'pages#show', as: :page
 end
+
+require 'spree/core'
+
+Spree::Core::Engine.routes.draw do
+  if (
+    Spree::Auth::Engine.frontend_available? &&
+    Spree::Auth::Config.draw_frontend_routes
+  )
+    devise_scope :spree_user do
+      get '/account/logout', to: 'user_sessions#destroy'
+    end
+  end
+end
