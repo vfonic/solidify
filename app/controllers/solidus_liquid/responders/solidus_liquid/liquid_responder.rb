a0f24449bd134ef2a44108e91183150746e4839e
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-09 17:35:02 +0700

    Don't render in LiquidResponder
    * It renders unnecessarily and causes double rendering

--- app/controllers/solidus_liquid/responders/solidus_liquid/liquid_responder.rb
+++ app/controllers/solidus_liquid/responders/solidus_liquid/liquid_responder.rb
@@ -7,13 +7,12 @@ module SolidusLiquid
     include LiquidHelper
 
     def to_html
-      controller_name = controller.controller_name
-      action_name = DEFAULT_ACTIONS_FOR_VERBS[request.request_method_symbol]
-      controller_action = "#{controller_name}##{action_name}"
       liquid_assigns = controller.instance_variable_get(:@liquid_assigns)
       liquid_assigns['form'] = resource
 
-      render controller_action_to_liquid_file_path(resource, controller_action)
+      controller_name = controller.controller_name
+      action_name = DEFAULT_ACTIONS_FOR_VERBS[request.request_method_symbol]
+      controller_action = "#{controller_name}##{action_name}"
     end
   end
 end
