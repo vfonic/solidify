56fba2bdd7c56b68f43d26508a07f471f2f4c283
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-11 14:58:48 +0700

    Add RegistrationsController

---
+++ app/controllers/solidus_liquid/registrations_controller.rb
@@ -0,0 +1,22 @@
+module SolidusLiquid
+  class RegistrationsController < ::Devise::RegistrationsController
+    include SolidusLiquid::Liquid
+
+    def set_liquid_assigns
+      @liquid_assigns = {
+        'canonical_url' => "#{request.base_url}#{new_customer_registration_path}",
+        'page_title' => 'Create Account',
+        'template' => 'page'
+      }
+    end
+
+    before_action :configure_sign_up_params, only: [:create]
+
+    protected
+
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up,
+                                        keys: [:first_name, :last_name])
+    end
+  end
+end
--- app/controllers/solidus_liquid/sessions_controller.rb
+++ app/controllers/solidus_liquid/sessions_controller.rb
@@ -1,12 +1,10 @@
 module SolidusLiquid
   class SessionsController < ::Devise::SessionsController
-    include ::SolidusLiquid::Liquid
+    include SolidusLiquid::Liquid
 
     def set_liquid_assigns
       @liquid_assigns = {
         'canonical_url' => "#{request.base_url}#{new_customer_session_path}",
-        # 'canonical_url' => "#{request.base_url}#{new_spree_user_session_path}",
-        # 'form' => @form,
         'page_title' => 'Log in',
         'template' => 'page'
       }
