8501b79a34c92de36c7635dd140a41b0bed99613
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-20 16:12:23 +0700

    Use product_decorator instead of c/p whole product model class; Ensure decorators are loaded in the engine

--- lib/solidus_liquid/engine.rb
+++ lib/solidus_liquid/engine.rb
@@ -10,5 +10,13 @@ module SolidusLiquid
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
   end
 end
