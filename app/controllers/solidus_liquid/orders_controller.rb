47e31df98d5ccb8ca81a52590c5bfd740ef25485
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-21 13:13:17 +0700

    Add TODO for orders controller

--- app/controllers/solidus_liquid/orders_controller.rb
+++ app/controllers/solidus_liquid/orders_controller.rb
@@ -4,8 +4,11 @@ module SolidusLiquid
     def populate
       @order   = current_order(create_order_if_necessary: true)
       variant  = Spree::Variant.find(params[:id])
+      # quantity = params[:quantity].present? ? params[:quantity].to_i : 1
 
       begin
+        # TODO: check that this works when quantity is not set in the form
+        # @line_item = @order.contents.add(variant, quantity)
         @line_item = @order.contents.add(variant, params[:quantity])
       rescue ActiveRecord::RecordInvalid => e
         @order.errors.add(:base, e.record.errors.full_messages.join(", "))
