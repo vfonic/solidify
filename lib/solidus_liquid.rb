ae7b5d1fac5c27354656cfe46cd0d0d6c3ea796a
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-27 11:45:57 +0700

    Fix rubocop

--- lib/solidus_liquid.rb
+++ lib/solidus_liquid.rb
@@ -10,30 +10,32 @@ require 'rails-settings-cached'
 require 'redis'
 require 'sass'
 
-require "set"
-require "zlib"
-require "active_support/core_ext/module/attribute_accessors"
+require 'set'
+require 'zlib'
+require 'active_support/core_ext/module/attribute_accessors'
 require 'active_record/errors'
 
 module ActiveRecord
   class Migration
     class << self
-      # Raises <tt>ActiveRecord::PendingMigrationError</tt> error if any migrations are pending.
+      # Raises <tt>ActiveRecord::PendingMigrationError</tt> error if any
+      # migrations are pending.
       def check_pending!(connection = Base.connection)
-        if ActiveRecord::Migrator.needs_migration?(connection)
-          raise StandardError.new(
-            "Migrations are pending. To resolve this issue, run:\n\n        bin/rails db:migrate RAILS_ENV=#{::Rails.env}\n\n\
-            Missing migrations: #{ActiveRecord::Migrator.missing_migrations}\n\n"
-          )
-        end
+        return unless ActiveRecord::Migrator.needs_migration?(connection)
+        raise StandardError,
+              "Migrations are pending. To resolve this issue, run:\n\n\
+          bin/rails db:migrate RAILS_ENV=#{::Rails.env}\n\n\
+              Missing migrations: \
+              #{ActiveRecord::Migrator.missing_migrations}\n"
       end
     end
   end
 
-  class Migrator#:nodoc:
+  class Migrator #:nodoc:
     class << self
       def missing_migrations(connection = Base.connection)
-        migrations(migrations_paths).collect(&:version) - get_all_versions(connection)
+        migrations(migrations_paths).collect(&:version) -
+          get_all_versions(connection)
       end
     end
   end
