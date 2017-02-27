ed51a0933069b2cb17b15e378bb0377e0311f02a
Viktor Fonic <viktor.fonic@gmail.com>
2017-02-27 11:54:38 +0700

    Remove dummy app; Run default travis script command

--- spec/dummy/db/migrate/20170227034354_remove_considered_risky_from_spree_orders.spree.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree (originally 20150313140507)
-class RemoveConsideredRiskyFromSpreeOrders < ActiveRecord::Migration[4.2]
-  def change
-    remove_column :spree_orders, :considered_risky
-  end
-end
--- spec/dummy/db/migrate/20170227034437_add_reimbursement_id_index_to_spree_refunds.spree.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree (originally 20160718205859)
-class AddReimbursementIdIndexToSpreeRefunds < ActiveRecord::Migration[4.2]
-  def change
-    add_index(:spree_refunds, :reimbursement_id)
-  end
-end
