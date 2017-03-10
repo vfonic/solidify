aad9bbca71e6aa1be28dc6cf3db518f8a679b191
Viktor Fonic <viktor.fonic@gmail.com>
2017-03-10 16:17:23 +0700

    WIP: StringFilters specs

--- app/filters/solidus_liquid/string_filters.rb
+++ app/filters/solidus_liquid/string_filters.rb
@@ -1,10 +1,36 @@
 module SolidusLiquid
   module StringFilters
+    def camelcase(string)
+      string.underscore.camelize
+    end
+
     def handle(string)
       string.try(:parameterize)
     end
 
     alias handleize handle
+
+    def md5(string)
+      Digest::MD5.hexdigest(string)
+    end
+
+    def sha1(string)
+      Digest::SHA1.hexdigest(string)
+    end
+
+    def sha256(string)
+      Digest::SHA256.hexdigest(string)
+    end
+
+    def hmac_sha1(data, secret)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'),
+                              secret.encode('ASCII'), data.encode('ASCII'))
+    end
+
+    def hmac_sha256(data, secret)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'),
+                              secret.encode('ASCII'), data.encode('ASCII'))
+    end
 
     def pluralize(input, singular, plural)
       input == 1 ? singular : plural
--- spec/filters/solidus_liquid/string_filters_spec.rb
+++ spec/filters/solidus_liquid/string_filters_spec.rb
@@ -1,9 +1,79 @@
+require 'rails_helper'
+
 module SolidusLiquid
   RSpec.describe 'StringFilters', type: :helper do
     subject { render_liquid(template, assigns, strict) }
 
     let(:assigns) { Hash.new }
     let(:strict) { true }
+
+    describe '#append' do
+      let(:expected) { 'apple and mango' }
+      it_behaves_like 'filter', '"apple"', 'append', '" and mango"'
+    end
+
+    describe '#camelcase' do
+      let(:expected) { 'ComingSoon' }
+      it_behaves_like 'filter', '"coming-soon"', 'camelcase'
+    end
+
+    describe '#capitalize' do
+      let(:expected) { 'Capitalize me' }
+      it_behaves_like 'filter', '"capitalize me"', 'capitalize'
+    end
+
+    describe '#downcase' do
+      let(:expected) { 'downcase me' }
+      it_behaves_like 'filter', '"DOWNCASE ME"', 'downcase'
+    end
+
+    describe '#escape' do
+      let(:expected) { '&lt;p&gt;test&lt;/p&gt;' }
+      it_behaves_like 'filter', '"<p>test</p>"', 'escape'
+    end
+
+    describe '#handle' do
+      let(:expected) { '100-m-ms' }
+      it_behaves_like 'filter', '"100% M & Ms!!!"', 'handle'
+    end
+
+    describe '#handleize' do
+      let(:expected) { '100-m-ms' }
+      it_behaves_like 'filter', '"100% M & Ms!!!"', 'handleize'
+    end
+
+    describe '#md5' do
+      let(:expected) { Digest::MD5.hexdigest('arthur.dent@example.com') }
+      it_behaves_like 'filter', '"arthur.dent@example.com"', 'md5'
+    end
+
+    describe '#sha1' do
+      let(:expected) { Digest::SHA1.hexdigest('arthur.dent@example.com') }
+      it_behaves_like 'filter', '"arthur.dent@example.com"', 'sha1'
+    end
+
+    describe '#sha256' do
+      let(:expected) { Digest::SHA256.hexdigest('arthur.dent@example.com') }
+      it_behaves_like 'filter', '"arthur.dent@example.com"', 'sha256'
+    end
+
+    describe '#hmac_sha1' do
+      let(:expected) do
+        OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha1'),
+                                'secret_key', 'arthur.dent@example.com')
+      end
+      it_behaves_like('filter', '"arthur.dent@example.com"',
+                      'hmac_sha1', '"secret_key"')
+    end
+
+    describe '#hmac_sha256' do
+      let(:expected) do
+        OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha256'),
+                                'secret_key', 'arthur.dent@example.com')
+      end
+      it_behaves_like('filter', '"arthur.dent@example.com"',
+                      'hmac_sha256', '"secret_key"')
+    end
 
     describe '#size' do
       let(:expected) { '6' }
