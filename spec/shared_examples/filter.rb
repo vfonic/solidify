9cee8611441bededf1a49e616dbc4fc3f13f9381
Viktor Fonic <viktor.fonic@gmail.com>
2017-03-14 16:19:56 +0700

    Fix broken specs
    Why:
    
    * The specs were failing because `expected_param` was always defined
      even when it wasn't passed to shared example
    
    How:
    
    * Allow `expected_param` to be empty string, but disallow nil

--- spec/shared_examples/filter.rb
+++ spec/shared_examples/filter.rb
@@ -5,7 +5,7 @@ RSpec.shared_examples 'filter' do |variable, method, parameter, expected_param|
   end
 
   it 'returns correct result' do
-    if defined? expected_param
+    unless expected_param.nil?
       expect(subject).to eq(expected_param)
     else
       expect(subject).to eq(expected)
